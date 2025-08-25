13a6dae3cb3af78f74076c92054dc01fcae2e707
Slade Harker <slade_harker@hotmail.com>
2025-08-25 10:39:29 +1000

    fix env names in python script

--- src/python_event_test.py
+++ src/python_event_test.py
@@ -1,3 +1,5 @@
+# pip install rudder-sdk-python
+
 import os
 import rudderstack.analytics as rudder_analytics
 from dotenv import load_dotenv
@@ -6,7 +8,7 @@ load_dotenv()
 
 # RudderStack Write Key and Dataplane URL for the JS source we setup in RudderStack
 
-rudder_analytics.write_key = os.getenv("REACT_APP_RUDDERSTACK_PYTHON_WRITE_KEY")
+rudder_analytics.write_key = os.getenv("REACT_APP_RUDDERSTACK_WRITE_KEY")
 rudder_analytics.dataPlaneUrl = os.getenv("REACT_APP_RUDDERSTACK_DATAPLANE_URL")
 
 def on_error(error, events):
@@ -14,9 +16,8 @@ def on_error(error, events):
 
 rudder_analytics.on_error = on_error
 
-rudder_analytics.identify('1hKOmRA4GRlm', {
-    'email': 'bob@example.com',
-    'name': 'Bob Smith',
-    'friends': 2
+rudder_analytics.track('sxh@notread.com', 'bp_add_ach_start', {
+    'bp_variant': 'SAH',
+    'care_type': 'CHSP'
 })  
 
